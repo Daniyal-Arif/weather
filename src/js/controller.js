3b3441dcba87eb6a67e8cf8e6ab4f2af33083dfb
Daniyal-Arif <daniyalmarif@gmail.com>
2021-11-02 01:43:36 -0400

    first markup test

--- src/js/controller.js
+++ src/js/controller.js
@@ -67,14 +67,16 @@ const controlSearchResults = async function (city) {
     );
     const data = await res.json();
     if (!res.ok) throw new Error("city does not exist");
-    console.log(data.list);
+
+    // refactor objects
     const newData = data.list.map((obj) => {
       return {
-        date: obj.dt_txt,
+        date: obj.dt_txt.split(" ")[0],
         atmosphere: {
           feelsLike: Math.round(obj.main.feels_like - 272.15),
           humidity: obj.main.humidity,
           pressure: obj.main.pressure,
+          wind: obj.wind.speed,
         },
         temperature: {
           temp: Math.round(obj.main.temp - 272.15),
@@ -82,12 +84,117 @@ const controlSearchResults = async function (city) {
           tempMin: Math.round(obj.main.temp_min - 272.15),
         },
         description: obj.weather[0].description,
-        iconTag: obj.weather[0].main,
+        iconTag: obj.weather[0].main.toLowerCase(),
       };
     });
+
+    // filter objects based on day
+    const currentDate = new Date();
+    console.log(currentDate);
+    currentDate.setDate(currentDate.getDate() + 3);
+    console.log(currentDate);
+    let dayOne = newData.filter((obj) => {
+      console.log(obj.date);
+      return (
+        obj.date ===
+        `${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${
+          currentDate.getDay() < 10
+            ? "0" + currentDate.getDay()
+            : currentDate.getDay()
+        }`
+      );
+    });
     console.log(newData);
-    // refactor data received
-    const weather = {};
+    console.log(dayOne);
+
+    const markup = `
+    <div class="forecast">
+            <ul class="cards">
+              <li class="card card-active">
+                <a href="#" class="card-link">
+                  <p class="date">${dayOne[0].date}</p>
+                  <img
+                    class="logo"
+                    src="img/icon-pack/weather-114/svg/wind.svg"
+                    alt=""
+                  />
+                  <p class="temperature">
+                    <span class="max-temperature">33&#176;C</span>/
+                    <span class="min-temperature">27&#176;C</span>
+                  </p>
+                  <p class="weather-description">Mostly cloudy</p>
+                </a>
+              </li>
+
+              <li class="card">
+                <a href="#" class="card-link">
+                  <p class="date">Friday 17</p>
+                  <img
+                    class="logo"
+                    src="img/icon-pack/weather-114/svg/cold-temperature.svg"
+                    alt=""
+                  />
+                  <p class="temperature">
+                    <span class="max-temperature">33&#176;C</span>/
+                    <span class="min-temperature">27&#176;C</span>
+                  </p>
+                  <p class="weather-description">Mostly cloudy</p>
+                </a>
+              </li>
+
+              <li class="card">
+                <a href="#" class="card-link">
+                  <p class="date">Friday 17</p>
+                  <img
+                    class="logo"
+                    src="img/icon-pack/weather-114/svg/wind.svg"
+                    alt=""
+                  />
+                  <p class="temperature">
+                    <span class="max-temperature">33&#176;C</span>/
+                    <span class="min-temperature">27&#176;C</span>
+                  </p>
+                  <p class="weather-description">Mostly cloudy</p>
+                </a>
+              </li>
+
+              <li class="card">
+                <a href="#" class="card-link">
+                  <p class="date">Friday 17</p>
+                  <img
+                    class="logo"
+                    src="img/icon-pack/weather-114/svg/cold-temperature.svg"
+                    alt=""
+                  />
+                  <p class="temperature">
+                    <span class="max-temperature">33&#176;C</span>/
+                    <span class="min-temperature">27&#176;C</span>
+                  </p>
+                  <p class="weather-description">Mostly cloudy</p>
+                </a>
+              </li>
+
+              <li class="card">
+                <a href="#" class="card-link">
+                  <p class="date">Friday 17</p>
+                  <img
+                    class="logo"
+                    src="img/icon-pack/weather-114/svg/clouds.svg"
+                    alt=""
+                  />
+                  <p class="temperature">
+                    <span class="max-temperature">33&#176;C</span>/
+                    <span class="min-temperature">27&#176;C</span>
+                  </p>
+                  <p class="weather-description">Mostly cloudy</p>
+                </a>
+              </li>
+            </ul>
+          </div>
+    `;
+    document
+      .querySelector(".section-weather")
+      .insertAdjacentHTML("afterbegin", markup);
   } catch (err) {
     console.log(err);
   }
